dd7bbe32f812541e5a3f8305eb116fb20a5f9219
Dave Meehan <dave_meehan@replicated.co.uk>
2020-05-23 18:21:50 +0100

    Refactor regexp to use named captures

--- Text/ClearText_Parser_Links.php
+++ Text/ClearText_Parser_Links.php
@@ -1,41 +1,42 @@
 <?php
 
 class ClearText_Parser_Links extends TextParser_Parser {
-	
+
 	public $regexp = '/
-						(?<!\\\\)			# not preceeded by escape
-						\[					# square bracket
-						(
-							(
+						(?<!\\\\)								# not preceeded by escape
+						\[											# square bracket
+						(?P<url>											# 1
+							(?P<namespace>				# 2
 								(?:\w[\w\d\-_]*:)?	# namespace (optional)
 							)
-							(
-								(.*?)		# uri (optional)
-								(?:\#(.+?))?	# target (optional)
-								(?:\?(.*?))?	# query params (optional)
+							(?P<linktext>					# 3
+								(?P<uri>.*?)				# 4 uri (optional)
+								(?:\#(?P<target>.+?))?				# 5 target (optional)
+								(?:\?(?P<query>.*?))?				# 6 query params (optional)
 							)
 						)
-						(?:\|(.*?))?		# supplement (optional)
-						\]					# square bracket
-						(?!:)				# not followed by colon (a link reference)
+						(?:\|(?P<supplement>.*?))?						# 7 supplement (optional)
+						\]											# square bracket
+						(?!:)										# not followed by colon (a link reference)
 					/x';
-					
+
 	public function onMatch($match) {
-		if (!empty($match[2])) {
-			$namespace = explode(':', $match[2]);
+
+		if (!empty($match['namespace'])) {
+			$namespace = explode(':', $match['namespace']);
 			array_pop($namespace);	// always pop the last empty element
 		} else {
 			$namespace = array();
 		}
 		return $this->addToken(TextParser::SPAN, array(
 									'namespace' => $namespace,
-									'uri' => $match[4],
-									'target' => $match[5],
-									'query' => $match[6],
-									'supplement' => $match[7],
-									'linktext' => $match[3]));
+									'uri' => $match['uri'],
+									'target' => $match['target'],
+									'query' => $match['query'],
+									'supplement' => $match['supplement'],
+									'linktext' => $match['linktext']));
 	}
 
 }
-	
+
 ?>
